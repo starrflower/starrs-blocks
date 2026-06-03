1047b505265656006e00fc1f264a97a88a23f36e
StarrFlower <starrflower@proton.me>
2026-06-02 20:14:16 -0700

    initial commit

---
+++ src/main/java/starrflower/blocks/StarrSBlocksDataGenerator.java
@@ -0,0 +1,11 @@
+package starrflower.blocks;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class StarrSBlocksDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
